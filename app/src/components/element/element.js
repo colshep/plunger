b5cbcf7647d37d90bc0b831eab74c7a497cb7e59
colshep <colshep@163.com>
2021-11-19 15:02:33 +0800

    init

--- app/src/components/element/element.js
+++ app/src/components/element/element.js
@@ -2,6 +2,6 @@ import Vue from 'vue'
 import Element from 'element-ui'
 // 用element-variables.scss编译很慢，放弃
 // import '../element-variables.scss'
-import 'theme/index.css'
+import '@/components/element/theme/index.css'
 
 Vue.use(Element)
